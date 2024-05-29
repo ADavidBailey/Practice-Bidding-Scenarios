143abafa8da776cd5874df073687f8778f2f90f4
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-05-29 06:11:11 -0500

    new pbn's

--- adbRotate.py
+++ adbRotate.py
@@ -1,7 +1,6 @@
 import os
-
+    
 # Directory containing the input files
-directory_path = './pbn'
 
 # Regular expression pattern to match text enclosed in backticks spanning multiple lines
 #pattern = r'`(.*?)`'
@@ -10,66 +9,55 @@ directory_path = './pbn'
 
 
 def rotate_deal(file_path):
-    with open(file_path, 'r') as file:
+     with open(file_path, 'r') as file:
         content = file.read()
         processed_text = rotate_hand(content)
         output_file_path = os.path.splitext(file_path)[0] + ".pbn"
         output_file_path = os.path.join("./pbnr4", output_file_path).replace('\\', '/')
+        print(output_file_path)
 
         with open(output_file_path, 'w') as output_file:
-            # Save the processed text to the .dlr file
-            print(output_file.name, "rotated")
+            # Save the processed text to the ./pbnr4/ ... .pbn file
+            print(output_file.name, " rotated")
             output_file.write(processed_text)
 
 
 def rotate_hand(extracted_text):
     processed_text = []
     lines = extracted_text.split('\n')
-    #prefix_mapping = {"N:": "E:", "E:": "S:", "S:": "W:", "W:": "N:"}
     processed_text = []
+    bN = 0
 
     for line in lines:
-        if line.startswith("[Board "):
-            bNumber = bNumber + 1       # increment board Number
-            bDi = bNumber % 4           # board number index of the board dealer
-            rDi = (dDi + bDi) % 4       # rotated dealer index (the dDi for North is 0)
-            rDeal = rotation[rDi]       # the first char of the Deal string
         if line.startswith("[Dealer "):
-            line[9] = dDealer           # set the designated dealer - 10th character
+            line = line[:9] + dDealer + '"]'
+            print(line)
         if line.startswith("[Deal "):
-            line[7] = rDeal             # rotated deal - 8th character
-
-        #   prefix = line[7:9]  # Extract the prefix
-        #   replacement = prefix_mapping.get(prefix, None)
-        #    #if replacement:
-        #    #    processed_text.append(line.replace(prefix, replacement, 1))
-        #    #else:
-        #    #    processed_text.append(line)
-        #    processed_text.append(line.replace(prefix, replacement, 1))
-        #else:
-        
-        
+            bN = bN + 1                 # increment board Number
+            bDi = (bN % 4) - 1          # rotated dealer index (the dDi for North is 0)
+            rDeal = rotation[bDi]       # the first char of the Deal string
+            line = line[:6] + rDeal + line[7:]              # rotated deal - 8th character
+            print(line)
         processed_text.append(line)
     return '\n'.join(processed_text)
 
 
 # List all files in the directory
-for filename in os.listdir(directory_path):
+in_directory_path = './pbnTest'     # Read pbn files
+out_directory_path = './pbnr4'      # Write r4pbn files
+testCount = 0
+rotation = "NESW"
+for filename in os.listdir(in_directory_path):
     
-    rotation = "NESW"
-    print(filename)
-    dDealer = filename[-5]               # designated dealer is the last character of the .dlr & .pbn filename
-    print(dDealer)
-    dDi = rotation.index(dDealer)        # designated dealer index, value is  0 to 3
-    bNumber = 0                          # initialize the board number (counting is easier than extracting it)
-    testCount = 0
 
-
-    file_path = os.path.join(directory_path, filename)
+    file_path = os.path.join(in_directory_path, filename)
     # Check if it's a file
-    if os.path.isfile(file_path) and (filename.endswith('.pbn') and (filename.find('-rotated') == -1)):
+    if os.path.isfile(file_path) and (filename.endswith('.pbn')):
         testCount = testCount + 1
-        if testCount > 40:
+        if testCount > 5:
             break
+        dDealer = filename[-5]               # designated dealer is the last character of the .dlr & .pbn filename
+        dDi = rotation.index(dDealer)        # designated dealer index, value is  0 to 3
         print(file_path)
+        print(filename," Dealer=",dDealer," Dealer Index=",str(dDi))
         rotate_deal(file_path)
