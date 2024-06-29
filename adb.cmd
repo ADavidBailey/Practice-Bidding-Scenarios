5115c0aa9c95a2968aff32b4c381cfb93688b741
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-06-29 08:49:16 -0500

    import leveling code

---
+++ adb.cmd
@@ -0,0 +1,3 @@
+P:\dealer P:\dlr\Dealer-Losing-Trick-Count-S.dlr -s=185433806 >P:\pbn\Dealer-Losing-Trick-Count-S.pbn
+P:\dealer P:\dlr\Dealer-WeXOppsWeak2-E.dlr -s=2442310641 >P:\pbn\Dealer-WeXOppsWeak2-E.pbn
+P:\dealer P:\dlr\Dealer-WeXOppsPreempt-E.dlr -s=2306468337 >P:\pbn\Dealer-WeXOppsPreempt-E.pbn
