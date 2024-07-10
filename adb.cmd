4271bb15c227500f61de3c209d6c72d4eca2839b
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-07-10 10:41:12 -0500

    delete adb.cmd

--- adb.cmd
+++
@@ -1,3 +0,0 @@
-P:\dealer P:\dlr\Dealer-Losing-Trick-Count-S.dlr -s=185433806 >P:\pbn\Dealer-Losing-Trick-Count-S.pbn
-P:\dealer P:\dlr\Dealer-GoodBad2N-N.dlr -s=3693421770 >P:\pbn\Dealer-GoodBad2N-N.pbn
-P:\dealer P:\dlr\Dealer-ForcingPass-N.dlr -s=819714614 >P:\pbn\Dealer-ForcingPass-N.pbn
