020e8bf6b29ebab69418c70f07715149c25f207a
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-07-20 18:21:06 -0500

    get stats for rule of 16

--- py/getResult.py
+++ py/getResult.py
@@ -2,9 +2,9 @@
 
 import os
 
-def scan_for_OptimumResult():
+def scan_for_Score():
 
-    input_file  = os.path.join(os.path.expanduser("~"), 'Practice-Bidding-Scenarios/BBA/rule18.pbn')
+    input_file  = os.path.join(os.path.expanduser("~"), 'Practice-Bidding-Scenarios/BBA/rule15.pbn')
     print(input_file)
 
     with open(input_file, 'r') as i_file:
@@ -12,39 +12,25 @@ def scan_for_OptimumResult():
         content = i_file.read()
         lines = content.strip().split('\n')
         
-        captureNextLine = False
-        game = False
         nDeals = 0
         nGames = 0
         for line in lines:
-            if captureNextLine == True:
-                game = False
-                nDeals = nDeals + 1
-                words = line.split()
-                if words[0] == 'N':
-                    if words[1] == 'NT':
-                        if words[2]>'8':
-                            game = True
-                    elif words[1] == 'S' or words[1] == 'H':
-                        if words[2] > '9':
-                            game = True
-                    elif words[1] == 'D' or words[1] == 'C':
-                        if words[2] > '10':
-                            game = True
-                if game == True:
-                    nGames = nGames + 1
-                captureNextLine = False
-                if game == True:
-                    print(str(nDeals) + ': ' + line + ' game')
-            if line.startswith('[OptimumResult'):
-                captureNextLine = True
+            if line.startswith('[Board'):
+                board = line[8:-2]
+            if line.startswith('[Declarer'):
+                declarer = line[11:-2]
+            if line.startswith('[Contract'):
+                contract = line[11:-2]
+            if line.startswith('[Score'):
+                if declarer == 'N' or declarer == 'S': 
+                    nDeals = nDeals + 1
+                    score = line[11:-2]
+                    print(board + ' ' + declarer + ' ' + contract + ' ' + score)
+                    if int(score)>150:
+                        nGames = nGames + 1
         print('nDeals = ' + str(nDeals))
         print('nGames = ' + str(nGames))
         print('%Games = ' + str(nGames/nDeals) + '%') 
 
-def main():
 
-    scan_for_OptimumResult()
-
-if __name__ == "__main__":
-    main()
+scan_for_Score()
