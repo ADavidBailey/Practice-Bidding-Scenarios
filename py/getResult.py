c9ca150d46e39978dc49ac829d2de2475342cdbc
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-07-21 07:03:19 -0500

    Updated Rule of 16 tests

--- py/getResult.py
+++ py/getResult.py
@@ -1,36 +1,28 @@
-# Reads all .pbn files from directory /pbn and makes the statistic lines comments
+input_file  = '/Users/adavidbailey/Practice-Bidding-Scenarios/BBA/rule15.pbn'
+print(input_file)
 
-import os
-
-def scan_for_Score():
-
-    input_file  = os.path.join(os.path.expanduser("~"), 'Practice-Bidding-Scenarios/BBA/rule15.pbn')
-    print(input_file)
-
-    with open(input_file, 'r') as i_file:
-        # Split the string into individual lines
-        content = i_file.read()
-        lines = content.strip().split('\n')
+with open(input_file, 'r') as i_file:
+    # Split the string into individual lines
+    content = i_file.read()
+    lines = content.strip().split('\n')
         
-        nDeals = 0
-        nGames = 0
-        for line in lines:
-            if line.startswith('[Board'):
-                board = line[8:-2]
-            if line.startswith('[Declarer'):
-                declarer = line[11:-2]
-            if line.startswith('[Contract'):
-                contract = line[11:-2]
-            if line.startswith('[Score'):
-                if declarer == 'N' or declarer == 'S': 
-                    nDeals = nDeals + 1
-                    score = line[11:-2]
-                    print(board + ' ' + declarer + ' ' + contract + ' ' + score)
-                    if int(score)>150:
-                        nGames = nGames + 1
-        print('nDeals = ' + str(nDeals))
-        print('nGames = ' + str(nGames))
-        print('%Games = ' + str(nGames/nDeals) + '%') 
-
-
-scan_for_Score()
+    nDeals = 0
+    nGames = 0
+    print('board declarer contract score')
+    for line in lines:
+        if line.startswith('[Board'):
+            board = line[8:-2]
+        if line.startswith('[Declarer'):
+            declarer = line[11:-2]
+        if line.startswith('[Contract'):
+            contract = line[11:-2]
+        if line.startswith('[Score'):
+            score = line[11:-2]
+            if declarer == 'N' or declarer == 'S': 
+                nDeals = nDeals + 1
+                if int(score)>150:
+                    nGames = nGames + 1
+            print(board + ' ' + declarer + ' ' + contract + ' ' + score)
+    print('nDeals = ' + str(nDeals))
+    print('nGames = ' + str(nGames))
+    print('%Games = ' + str(nGames/nDeals) + '%') 
