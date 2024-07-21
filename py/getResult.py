8b96d6714d72ec7f69b703019ad3017bd2a0c655
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-07-21 13:30:23 -0500

    Update getResults

--- py/getResult.py
+++ py/getResult.py
@@ -1,4 +1,4 @@
-input_file  = '/Users/adavidbailey/Practice-Bidding-Scenarios/BBA - Drury.pbn'
+input_file  = '/Users/adavidbailey/Practice-Bidding-Scenarios/BBA/Drury.pbn'
 print(input_file)
 
 with open(input_file, 'r') as i_file:
@@ -8,6 +8,8 @@ with open(input_file, 'r') as i_file:
         
     nDeals = 0
     nGames = 0
+    notes = {}
+    this_note = ''
     print('board declarer contract score')
     for line in lines:
         if line.startswith('[Board'):
@@ -23,6 +25,24 @@ with open(input_file, 'r') as i_file:
                 if int(score)>150:
                     nGames = nGames + 1
             print(board + ' ' + declarer + ' ' + contract + ' ' + score)
+        if line.startswith('[Note'):
+            note = line[9:-2]
+            if note not in notes:
+                notes[note] = 0
+            notes[note] += 1
+            if this_note == '':
+                this_note = note
+            else:
+                this_note = this_note + ' | ' + note
+        if line.strip() == '':
+            print(board + ' ' + declarer + ' ' + contract + ' ' + score + ' | ' + this_note)
+            this_note = ''
+    print('Statistics for ' + input_file)
+    print(' ')
     print('nDeals = ' + str(nDeals))
     print('nGames = ' + str(nGames))
-    print('%Games = ' + str(nGames/nDeals) + '%') 
+    print('%Games = ' + str((nGames/nDeals) * 100) + '%')
+    for note in notes:
+        txt = '{:>5} ' + note
+        print(txt.format(str(notes[note])))
+                         
