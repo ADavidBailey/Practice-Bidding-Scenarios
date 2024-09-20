101afe81737d76cefd4381a89c1e91a2c6c80608
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-09-20 08:32:19 -0500

    test makeBBA.cmd

--- makeBBA.cmd
+++ makeBBA.cmd
@@ -1,2 +1,2 @@
-P:\BBA /D --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\21GF-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\2N_Smolen_Leveled-S.pbn --ARCHIVE_FILE C:\XXX\2N_Smolen_Leveled-S.bba
-P:\BBA /D --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\21GF-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Opps_2-Suited_Overcalls-S.pbn --ARCHIVE_FILE C:\XXX\Opps_2-Suited_Overcalls-S.bba
+P:\BBA /D --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\21GF-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\2N_Smolen_Leveled-S.pbn --ARCHIVE_FILE C:\XXX\2N_Smolen_Leveled-S.pbn
+P:\BBA /D --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\21GF-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Opps_2-Suited_Overcalls-S.pbn --ARCHIVE_FILE C:\XXX\Opps_2-Suited_Overcalls-S.pbn
