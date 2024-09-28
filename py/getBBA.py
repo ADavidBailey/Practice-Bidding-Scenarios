988e4a4e846d3cbc897b844b60aab6a96502f8cd
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-09-28 16:52:34 -0500

    Update bba files

--- py/getBBA.py
+++ py/getBBA.py
@@ -17,6 +17,8 @@ print("writing " + str(output_file))
 f = open(output_file, 'w')
 f.write(input_file+'\n')
 
+
+f.write('\n -- Sorted Board Number --\n')
 with open(input_file, 'r') as i_file:
     # Split the string into individual lines
     content = i_file.read()
@@ -25,17 +27,16 @@ with open(input_file, 'r') as i_file:
     nBoards = 0  
     nDeals = 0
     nGames = 0
-    notes = {}
+    results = {}
     this_note = ''
-    auctions = {}
     this_auction = ''
     auction = False
     par = ''
     optimum = False
-    f.write('board declarer contract score  par    | notes' + '\n')
+    f.write('board declarer contract score  par    | auction... notes' + '\n')
     for line in lines:
         if line.startswith('[Board'):
-            board = line[8:-2]
+            board_number = line[8:-2]
             nBoards += 1
         if line.startswith('[Declarer'):
             declarer = line[11:-2]
@@ -49,23 +50,9 @@ with open(input_file, 'r') as i_file:
                     nGames = nGames + 1
         if auction == True:
             if line.startswith('['):
-                # this marks the end of this auction; now add it to auctions, the 'dictionary of auctions'
-                this_auction = '-'.join(this_auction.split()).replace('Pass', 'P').replace('-P-P-P','')
-
-                # auctions start with a space so they will sort first
-                this_auction = ' ' + re.sub(r'-=\d=', '', this_auction)
-
-                if this_auction not in notes:
-                    notes[this_auction] = 0
-                notes[this_auction] += 1
-                
-                if this_note == '':
-                    this_note = this_auction
-                else:
-                    this_note = this_note + ' | ' + this_auction
-
-                # we're all done with this auction
-                auction = False      
+                # this marks the end of this auction; save the bidding.'
+                bidding = '-'.join(this_auction.split()).replace('Pass', 'P').replace('-P-P-P','')
+                auction = False
             else:
                 this_auction = this_auction + ' ' + line
         if line.startswith('[Auction'):
@@ -73,48 +60,29 @@ with open(input_file, 'r') as i_file:
             auction = True
         if line.startswith('[Note'):
             note = line[9:-2].capitalize()
-            if note not in notes:
-                notes[note] = 0
-            notes[note] += 1
-            if this_note == '':
-                this_note = note
-            else:
-                this_note = this_note + ' | ' + note
+            this_note = this_note + ' | ' + note
         if optimum == True:
             par = line.strip()
             optimum = False
         if line.startswith('[Optimum'):
             optimum = True
         if line.startswith('[Play'):
-            txt = board.rjust(5) + declarer.rjust(6) + contract.rjust(9) + score.rjust(9) + '  ' + par.ljust(7)
-            f.write(txt + '| ' + this_note + '\n')
+            # I've got everything needed; so; write it out.
+            result = bidding + ' ' + this_note
+            f.write(board_number.rjust(5) + declarer.rjust(6) + contract.rjust(9) + score.rjust(9) + '  ' + par.ljust(7) + ' ' + result + '\n')
+            
+            if result not in results:
+                results[result] = 1
+            else:
+                results[result] += 1
             this_note = ''
             this_auction = ''
-
+   
+    f.write('\n -- Sorted Bidding Sequences --\n')
+    for result in dict(sorted(results.items())):
+        txt = str(results[result])
+        f.write(txt.rjust(5) + '  ' + result +'\n')
+    
     f.write('\nSummary for ' + input_file + '\n')
     f.write('Deals N/S = ' + str(nDeals) + '\n')
     f.write('Games N/S = ' + str(nGames) + ' or ' + str(round(nGames/nDeals * 100, 2)) + '%\n')
-
-    f.write('\n -- Sorted Bidding Sequences --\n')
-
-    txt = ''
-    nDeals = 0
-    nNotes = 0
-    for note in dict(sorted(notes.items())):
-        if note[0] == ' ':
-            nDeals += notes[note]
-        else:
-            nNotes += notes[note]
-        txt = str(notes[note])
-        f.write(txt.rjust(5) + '  ' + note + '\n')
-
-        if nDeals == nBoards:
-            txt = (str(nDeals))
-            f.write('_____\n')
-            f.write(txt.rjust(5) + '  ' + 'TOTAL Deals\n\n')
-            f.write(' -- Sorted Notes --\n')
-            nDeals = 0
-
-    f.write('_____\n')       
-    txt = ('    ' + str(nNotes))
-    f.write(txt[-5:] + '  ' + 'TOTAL Notes\n\n')
