64ad8abed42499cc03f6f418a60b27e2df50b050
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-10-08 13:38:45 -0500

    update 50 bba files

--- updateBBA.cmd
+++ updateBBA.cmd
@@ -1,9 +1,149 @@
+del bba\2N_Smolen_Leveled.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\2N_Smolen_Leveled-S.pbn --ARCHIVE_FILE P:\bba\2N_Smolen_Leveled --AUTOCLOSE
 
-del bba\Splinters_After_Notrump.pbn
-C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Splinters_After_Notrump-S.pbn --ARCHIVE_FILE P:\bba\Splinters_After_Notrump --AUTOCLOSE
+del bba\Opps_2-Suited_Overcalls.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Opps_2-Suited_Overcalls-S.pbn --ARCHIVE_FILE P:\bba\Opps_2-Suited_Overcalls --AUTOCLOSE
 
-del bba\1N_GIB.pbn
-C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\1N_GIB-S.pbn --ARCHIVE_FILE P:\bba\1N_GIB --AUTOCLOSE
+del bba\SCS_1C_any_0-4_Resp.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\SCS_1C_any_0-4_Resp-S.pbn --ARCHIVE_FILE P:\bba\SCS_1C_any_0-4_Resp --AUTOCLOSE
 
-del bba\Rule_of_16.pbn
-C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Rule_of_16-N.pbn --ARCHIVE_FILE P:\bba\Rule_of_16 --AUTOCLOSE
+del bba\Splinters.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Splinters-S.pbn --ARCHIVE_FILE P:\bba\Splinters --AUTOCLOSE
+
+del bba\SCS_1C_3-Suit_Resp_5-7.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\SCS_1C_3-Suit_Resp_5-7-S.pbn --ARCHIVE_FILE P:\bba\SCS_1C_3-Suit_Resp_5-7 --AUTOCLOSE
+
+del bba\Soloway_Jump_Shift-Type-1.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Soloway_Jump_Shift-Type-1-S.pbn --ARCHIVE_FILE P:\bba\Soloway_Jump_Shift-Type-1 --AUTOCLOSE
+
+del bba\SCS_1C_any_5-7_Resp.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\SCS_1C_any_5-7_Resp-S.pbn --ARCHIVE_FILE P:\bba\SCS_1C_any_5-7_Resp --AUTOCLOSE
+
+del bba\Two-Way_New_Minor_Forcing_aka_xyNT.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Two-Way_New_Minor_Forcing_aka_xyNT-N.pbn --ARCHIVE_FILE P:\bba\Two-Way_New_Minor_Forcing_aka_xyNT --AUTOCLOSE
+
+del bba\Misfit_11-12.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Misfit_11-12-S.pbn --ARCHIVE_FILE P:\bba\Misfit_11-12 --AUTOCLOSE
+
+del bba\Stayman.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Stayman-N.pbn --ARCHIVE_FILE P:\bba\Stayman --AUTOCLOSE
+
+del bba\After_1M_2M.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\After_1M_2M-S.pbn --ARCHIVE_FILE P:\bba\After_1M_2M --AUTOCLOSE
+
+del bba\Soloway_Jump_Shift-Type-3.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Soloway_Jump_Shift-Type-3-S.pbn --ARCHIVE_FILE P:\bba\Soloway_Jump_Shift-Type-3 --AUTOCLOSE
+
+del bba\Trap_Pass_May.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Trap_Pass_Maybe.pbn --ARCHIVE_FILE P:\bba\Trap_Pass_May --AUTOCLOSE
+
+del bba\Splinters_By_Opener.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Splinters_By_Opener-S.pbn --ARCHIVE_FILE P:\bba\Splinters_By_Opener --AUTOCLOSE
+
+del bba\Minor_Slams.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Minor_Slams-S.pbn --ARCHIVE_FILE P:\bba\Minor_Slams --AUTOCLOSE
+
+del bba\Trap_Pass.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Trap_Pass-N.pbn --ARCHIVE_FILE P:\bba\Trap_Pass --AUTOCLOSE
+
+del bba\Two_Notrump.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Two_Notrump-S.pbn --ARCHIVE_FILE P:\bba\Two_Notrump --AUTOCLOSE
+
+del bba\Opps_Preemptive_Overcall.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Opps_Preemptive_Overcall-N.pbn --ARCHIVE_FILE P:\bba\Opps_Preemptive_Overcall --AUTOCLOSE
+
+del bba\Jacoby_Transfer.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Jacoby_Transfer-S.pbn --ARCHIVE_FILE P:\bba\Jacoby_Transfer --AUTOCLOSE
+
+del bba\Ned_Weak_Two_Leveled.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Ned_Weak_Two_Leveled-S.pbn --ARCHIVE_FILE P:\bba\Ned_Weak_Two_Leveled --AUTOCLOSE
+
+del bba\Any_5422_with_15-17.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Any_5422_with_15-17-S.pbn --ARCHIVE_FILE P:\bba\Any_5422_with_15-17 --AUTOCLOSE
+
+del bba\Vics_2C_Relay.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Vics_2C_Relay-S.pbn --ARCHIVE_FILE P:\bba\Vics_2C_Relay --AUTOCLOSE
+
+del bba\Slam_after_Stayman_or_Jacoby_w30plus.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Slam_after_Stayman_or_Jacoby_w30plus-S.pbn --ARCHIVE_FILE P:\bba\Slam_after_Stayman_or_Jacoby_w30plus --AUTOCLOSE
+
+del bba\Opps_Open_1N_15-17.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Opps_Open_1N_15-17-E.pbn --ARCHIVE_FILE P:\bba\Opps_Open_1N_15-17 --AUTOCLOSE
+
+del bba\Slam_after_Transfer.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Slam_after_Transfer-S.pbn --ARCHIVE_FILE P:\bba\Slam_after_Transfer --AUTOCLOSE
+
+del bba\Multi_2D.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Multi_2D-S.pbn --ARCHIVE_FILE P:\bba\Multi_2D --AUTOCLOSE
+
+del bba\Tislevoll_after_Opps_Preempt.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Tislevoll_after_Opps_Preempt-N.pbn --ARCHIVE_FILE P:\bba\Tislevoll_after_Opps_Preempt --AUTOCLOSE
+
+del bba\Minor_Suit_Opener.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Minor_Suit_Opener-S.pbn --ARCHIVE_FILE P:\bba\Minor_Suit_Opener --AUTOCLOSE
+
+del bba\Overcalls.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Overcalls-E.pbn --ARCHIVE_FILE P:\bba\Overcalls --AUTOCLOSE
+
+del bba\We_Overcall_1N.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\We_Overcall_1N-E.pbn --ARCHIVE_FILE P:\bba\We_Overcall_1N --AUTOCLOSE
+
+del bba\Gavin_3_Under_Invitational_Jump.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Gavin_3_Under_Invitational_Jump-N.pbn --ARCHIVE_FILE P:\bba\Gavin_3_Under_Invitational_Jump --AUTOCLOSE
+
+del bba\Gambling_3N.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Gambling_3N-S.pbn --ARCHIVE_FILE P:\bba\Gambling_3N --AUTOCLOSE
+
+del bba\Opps_Weak_Two.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Opps_Weak_Two-E.pbn --ARCHIVE_FILE P:\bba\Opps_Weak_Two --AUTOCLOSE
+
+del bba\Reverse_After_Two_Over_One.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Reverse_After_Two_Over_One-S.pbn --ARCHIVE_FILE P:\bba\Reverse_After_Two_Over_One --AUTOCLOSE
+
+del bba\McCabe_After_Weak_2.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\McCabe_After_Weak_2-S.pbn --ARCHIVE_FILE P:\bba\McCabe_After_Weak_2 --AUTOCLOSE
+
+del bba\Minor_Suit_Stayman.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Minor_Suit_Stayman-N.pbn --ARCHIVE_FILE P:\bba\Minor_Suit_Stayman --AUTOCLOSE
+
+del bba\XYZ.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\XYZ-S.pbn --ARCHIVE_FILE P:\bba\XYZ --AUTOCLOSE
+
+del bba\Jump_Overcalls.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Jump_Overcalls-E.pbn --ARCHIVE_FILE P:\bba\Jump_Overcalls --AUTOCLOSE
+
+del bba\Weak_NT_10-12.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Weak_NT_10-12-S.pbn --ARCHIVE_FILE P:\bba\Weak_NT_10-12 --AUTOCLOSE
+
+del bba\Negative_Double.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Negative_Double-S.pbn --ARCHIVE_FILE P:\bba\Negative_Double --AUTOCLOSE
+
+del bba\3_Under_Invitational_Jump.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\3_Under_Invitational_Jump-S.pbn --ARCHIVE_FILE P:\bba\3_Under_Invitational_Jump --AUTOCLOSE
+
+del bba\Minor_Suit_Opener_Inv_Raise.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Minor_Suit_Opener_Inv_Raise-S.pbn --ARCHIVE_FILE P:\bba\Minor_Suit_Opener_Inv_Raise --AUTOCLOSE
+
+del bba\Misfit_06-10.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Misfit_06-10-S.pbn --ARCHIVE_FILE P:\bba\Misfit_06-10 --AUTOCLOSE
+
+del bba\Misfit_13-Plus.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Misfit_13-Plus-S.pbn --ARCHIVE_FILE P:\bba\Misfit_13-Plus --AUTOCLOSE
+
+del bba\W2_X_XX.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\W2_X_XX-E.pbn --ARCHIVE_FILE P:\bba\W2_X_XX --AUTOCLOSE
+
+del bba\Namyats_Strong.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Namyats_Strong-S.pbn --ARCHIVE_FILE P:\bba\Namyats_Strong --AUTOCLOSE
+
+del bba\Gerber_after_NT.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Gerber_after_NT-S.pbn --ARCHIVE_FILE P:\bba\Gerber_after_NT --AUTOCLOSE
+
+del bba\2N_Smolen.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\2N_Smolen-S.pbn --ARCHIVE_FILE P:\bba\2N_Smolen --AUTOCLOSE
+
+del bba\Transfers_after_1M_X.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Transfers_after_1M_X-N.pbn --ARCHIVE_FILE P:\bba\Transfers_after_1M_X --AUTOCLOSE
+
+del bba\2N_and_Balanced.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\2N_and_Balanced-S.pbn --ARCHIVE_FILE P:\bba\2N_and_Balanced --AUTOCLOSE
