1a015234db177f70d6b0c2e7da5b8288e81b3c94
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-10-08 12:53:32 -0500

    update bba files

---
+++ updateBBA.cmd
@@ -0,0 +1,9 @@
+
+del bba\Splinters_After_Notrump.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Splinters_After_Notrump-S.pbn --ARCHIVE_FILE P:\bba\Splinters_After_Notrump --AUTOCLOSE
+
+del bba\1N_GIB.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\1N_GIB-S.pbn --ARCHIVE_FILE P:\bba\1N_GIB --AUTOCLOSE
+
+del bba\Rule_of_16.pbn
+C:\BBA\BBA --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --HAND P:\pbn\Rule_of_16-N.pbn --ARCHIVE_FILE P:\bba\Rule_of_16 --AUTOCLOSE
