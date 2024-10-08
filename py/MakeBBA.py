eeba19dfcf76416a4acd9670f8469253a5efa3ab
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-10-08 10:24:31 -0500

    -

--- py/MakeBBA.py
+++ py/MakeBBA.py
@@ -14,7 +14,7 @@ def process_file(files):
             archive = " --ARCHIVE_FILE P:\\bba\\" + pbn_file[:-6]
           
             print("del bba\\" + pbn_file[:-6]+ ".pbn", file = print_file)
-            print("C:\\BBA\\BBA" + cc1 + cc2 + " --DD 0 --SD 1 --AUTOBID" + hand + archive + " --AUTOCLOSE", file = print_file)
+            print("C:\\BBA\\BBA" + cc1 + cc2 + " --DD 0 --SD 1 --AUTOBID" + hand + archive + " --AUTOCLOSE\n", file = print_file)
             nfiles += 1
             #if nfiles > 1:
             #    break
