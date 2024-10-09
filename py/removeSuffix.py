bdab90e0ed2d1c84f7236282983c4ed525841928
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-10-09 13:42:49 -0500

    remove suffix from pbn files

--- py/removeSuffix.py
+++ py/removeSuffix.py
@@ -2,10 +2,10 @@
 
 import os
 n_files = 0
-PBS_DLR = os.path.join(os.path.expanduser("~"), "Practice-Bidding-Scenarios/dlr/")
+PBS_DLR = os.path.join(os.path.expanduser("~"), "Practice-Bidding-Scenarios/pbn-rotated-for-4-players/")
 current_directory_files = os.listdir(PBS_DLR)
 for file in current_directory_files:
-    if file.lower()[-6] == '-' and file.endswith('.dlr'):
+    if file.lower()[-6] == '-' and file.endswith('.pbn'):
     
         n_files += 1
         n_txt = str(n_files).ljust(4)
@@ -15,4 +15,4 @@ for file in current_directory_files:
         #if n_files > 2:
         #    break
 
-print(f"# RemoveSuffix is complete complete!")
+print(f"# Suffix removed from " + str(n_files) + " files.")
