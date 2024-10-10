3fba2a4ffbdd3000350ef98030c095b0d0c26493
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-10-09 22:00:03 -0500

    clean up

---
+++ test.cmd
@@ -0,0 +1,8 @@
+del bba\Soloway_Jump_Shift.pbn
+C:\BBA\BBA --HAND P:\pbn\Soloway_Jump_Shift.pbn --ARCHIVE_FILE P:\bba\Soloway_Jump_Shift --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --AUTOCLOSE
+
+del bba\Slam_after_Stayman_or_Jacoby_w30plus.pbn
+C:\BBA\BBA --HAND P:\pbn\Slam_after_Stayman_or_Jacoby_w30plus.pbn --ARCHIVE_FILE P:\bba\Slam_after_Stayman_or_Jacoby_w30plus --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --AUTOCLOSE
+
+del bba\Meckwell.pbn
+C:\BBA\BBA --HAND P:\pbn\Meckwell.pbn --ARCHIVE_FILE P:\bba\Meckwell --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --AUTOCLOSE
