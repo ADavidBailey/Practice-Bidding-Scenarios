79288470c16993aeda24c2fc20c83795a3ae6192
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-10-19 19:48:03 -0500

    Create bidding-sheets

--- build-scripts/MakeOneBiddingSheets.cmd
+++ build-scripts/MakeOneBiddingSheets.cmd
@@ -9,7 +9,7 @@ SET inputFileFolder=%~dp1
 :: Rotate the hands N/S - this will create a PNN file in the same folder
 :: with -NS added to the end of the filename:
 
-cscript Z:\Documents\BCScript\2022-06-18\SetDealerMulti.js %inputFilePath%.pbn NS North
+cscript \\Mac\Home\Documents\BCScript\2024-10-03\SetDealerMulti.js %inputFilePath%.pbn NS North
 
 :: Move the rotated file to the \bidding-sheets\ folder, and rename to remove the -NS suffix:
 
@@ -20,6 +20,6 @@ move /Y %inputFilePath%-NS.pbn ..\bidding-sheets\%inputFilename%.pbn
 :: Create a bidding sheets document, with the same name, but BiddingSheets added to the end,
 :: and file type .html:
 
-cscript Z:\Documents\BCScript\2022-06-18\BiddingSheets.wsf %inputFileFolder%..\bidding-sheets\%inputFilename%.pbn
+cscript \\Mac\Home\Documents\BCScript\2024-10-03\BiddingSheets.wsf %inputFileFolder%..\bidding-sheets\%inputFilename%.pbn
 
 :exitbat
