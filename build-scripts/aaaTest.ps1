55eade15927ae6ea9abe833e35f6305bcd252d9e
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-10-25 16:56:22 -0500

    testing

---
+++ build-scripts/aaaTest.ps1
@@ -0,0 +1,7 @@
+# A Script to Rule Them All
+
+# The number of files for PowerScript programs
+$global:nTestFiles = 4
+
+# /dlr > /pbn
+P:\build-scripts\abbTest.ps1
---
+++ build-scripts/abbTest.ps1
@@ -0,0 +1,4 @@
+# A Script to Rule Them All
+
+# The number of files for PowerScript programs
+Write-Host "Hello, World!" $global:nTestFiles
