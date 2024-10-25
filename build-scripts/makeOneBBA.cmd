2882ce54c29ae704d73e8e3b408574a69cf0f58b
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-10-25 07:54:53 -0500

    -

---
+++ build-scripts/makeOneBBA.cmd
@@ -0,0 +1,17 @@
+@echo off
+IF "%~1"=="" ECHO Provide PBN file as parameter 1 (without .pbn)
+IF "%~1"=="" goto exitbat
+
+SET inputFilePath=%~dpn1
+SET inputFilename=%~n1
+SET inputFileFolder=%~dp1
+
+::ECHO inputFilePath: %inputFilePath%
+::ECHO inputFilename: %inputFilename%
+::ECHO inputFileFolder: %inputFileFolder%
+
+:: Invoke BBA to create a bba file from a pbn file
+
+ECHO C:\BBA\BBA --HAND %inputFilePath% --ARCHIVE_FILE P:\bba\%inputFilename%.pbn --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\\BBA\\GIB-ADB.bbsa  --DD 0 --SD 1 --AUTOBID --AUTOCLOSE
+
+:exitbat
