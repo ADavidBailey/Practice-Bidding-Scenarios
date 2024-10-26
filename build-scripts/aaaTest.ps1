e2edae8fc8a7dda47c65f94590333e07a0265713
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-10-26 10:16:51 -0500

    testing

--- build-scripts/aaaTest.ps1
+++ build-scripts/aaaTest.ps1
@@ -14,7 +14,7 @@ echo "This is A Script to Rule Them All speaking, All done!"
 <#
 PS P:\build-scripts> ./aaatest.ps1
 This is A Script to Rule Them All speaking, I'm getting started!
-This is aCalled ps1 program speaking. nFiles = 1000
+This is a Called ps1 program speaking. nFiles = 1000
 This is A Script to Rule Them All speaking, All done!
 PS P:\build-scripts>
 #>
