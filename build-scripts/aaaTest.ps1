b1222c7c9beb58bc31a70b0d6cb79d03b9989375
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-10-26 10:09:46 -0500

    Testing

--- build-scripts/aaaTest.ps1
+++ build-scripts/aaaTest.ps1
@@ -2,6 +2,9 @@
 
 # The number of files for PowerScript programs
 $global:nTestFiles = 1000
+
+echo "This is A Script to Rule Them All speaking, I'm getting started!"
+
 # /dlr > /pbn
 P:\build-scripts\abbTest.ps1
 
