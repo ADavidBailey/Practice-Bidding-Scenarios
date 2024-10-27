0c25ef6d084141ba579c9416617ee0e9d05eaa75
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-10-27 05:44:41 -0500

    makeBBA

--- build-scripts/makeOneBBA.cmd
+++ build-scripts/makeOneBBA.cmd
@@ -3,9 +3,11 @@ IF "%~1"=="" ECHO Provide PBN file as parameter 1 (without .pbn)
 IF "%~1"=="" goto exitbat
 
 SET scenarioName=%~1
+SET inputFilePath=P:\pbn\%scenarioName%.pbn
+SET outputFilePath=P:\bba\%scenarioName%
 
 :: Invoke BBA to create a bba file from a pbn file
-del P:\bba\%scenarioName%.pbn
-BBA --HAND P:\bba\%scenarioName%.pbn --ARCHIVE_FILE P:\bba\%scenarioNamee% --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --AUTOCLOSE
+del %outputFilePath%
+BBA --HAND %inputFilePath% --ARCHIVE_FILE %outputFilePath% --CC1 C:\BBA\GIB-ADB.bbsa --CC2 C:\BBA\GIB-ADB.bbsa --DD 0 --SD 1 --AUTOBID --AUTOCLOSE
 
 :exitbat
