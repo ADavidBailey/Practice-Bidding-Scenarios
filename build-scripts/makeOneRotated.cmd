d6b2b8507d357a6b9b9ef0a730054f80ab02923f
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-10-26 23:34:44 -0500

    exclude gib1N

--- build-scripts/makeOneRotated.cmd
+++ build-scripts/makeOneRotated.cmd
@@ -1,27 +1,25 @@
 @echo off
-IF "%~1"=="" ECHO Provide PBN filename as parameter 1 (without .pbn)
-IF "%~1"=="" goto exitbat
+IF "%~1"=="" (
+    ECHO Provide PBN filename as parameter 1 (without .pbn)
+    goto exitbat
+)
 
 SET scenarioName=%~1
-
 SET tempFilePath=P:\temporary\temp
 SET inputFilePath=P:\pbn\%scenarioName%.pbn
 
-:: Rotate the hands N,E,S,W - this will create a rotated pbn in the temporary folder
-
 echo inputFilePath is %inputFilePath% tempFilePath is %tempFilePath%
+
+:: Rotate the hands N,E,S,W - this will create a rotated pbn in the temporary folder
 cscript S:\SetDealerMulti.js %inputFilePath% %tempFilePath%.pbn NESW Dealer
 
-:: Move the rotated file to the \pbn-rotated-for-4-players\ folder:
-
+:: Move the rotated file to the \pbn-rotated-for-4-players\ folder
 move /Y %tempFilePath%.pbn P:\pbn-rotated-for-4-players\%scenarioName%.pbn
 
 :: Rotate the hands N,E,S,W - this will create a rotated lin in the temporary folder
-
 cscript S:\SetDealerMulti.js %inputFilePath% %tempFilePath%.lin NESW Dealer NoPBN LIN
 
-:: Move the rotated file to the \lin-rotated-for-4-players\ folder:
-
+:: Move the rotated file to the \lin-rotated-for-4-players\ folder
 move /Y %tempFilePath%.lin P:\lin-rotated-for-4-players\%scenarioName%.lin
 
 :exitbat
