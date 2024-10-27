24123ab44ccc25a4ac5132bb8c4786611b6e79a3
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-10-27 05:07:43 -0500

    from Rick

--- build-scripts/filterOneScenario.cmd
+++ build-scripts/filterOneScenario.cmd
@@ -1,24 +1,30 @@
 @echo off
 
 ::
-::	This script will filter P:\bba\[{scenario}.pbn into P:\bba-filtered\ and P:\bba-filtered-out\
+:: This script will filter P:\bba\[{scenario}.pbn into P:\bba-filtered\ and P:\bba-filtered-out\
 ::
-::	It will get the filter string from AllScenarioFilters.cmd.
+:: It will get the filter string from AllScenarioFilters.cmd.
 ::
-::	p1 is the scenario name, and is required.  It should not have path or file extension.
+:: p1 is the scenario name, and is required.  It should not have path or file extension.
 ::
 
 if "%~1"=="" (
     echo Please provide a scenario name.
     exit /b
 )
-	
+
 set scenario=%~1
-call set filter=%%Filter_%scenario%%%
 
 call P:\build-scripts\DefineAllScenarioFilters.cmd
 
-cscript /nologo S:\Filter.js P:\bba\%scenario%.pbn %filter% P:\bba-filtered\%scenario%.pbn PDF
-cscript /nologo S:\Filter.js P:\bba\%scenario%.pbn %filter% P:\bba-filtered-out\%scenario%.pbn --INVERSE --PDF
+call set this_filter=%%Filter_%scenario%%%
+
+if %this_filter%=="" (
+echo %scenario% doesn't have a filter expression, so no filtered/unfiltered files will be generated
+exit /b
+)
+
+cscript /nologo S:\Filter.js P:\bba\%scenario%.pbn %this_filter% P:\bba-filtered\%scenario%.pbn --PDF
+cscript /nologo S:\Filter.js P:\bba\%scenario%.pbn %this_filter% P:\bba-filtered-out\%scenario%.pbn --INVERSE --PDF
 
 exit /b
