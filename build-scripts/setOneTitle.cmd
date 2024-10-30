763174393d9ed13d96afcc32361459b4e892829f
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-10-29 19:07:26 -0500

    BridgeCompose is in the path

--- build-scripts/setOneTitle.cmd
+++ build-scripts/setOneTitle.cmd
@@ -2,6 +2,6 @@
 IF "%~1"=="" EffHO Provide PBN file as parameter 1
 IF "%~1"=="" goto exitbat
 
-"C:\Program Files\Bridge Club Software\BridgeComposer\BridgeComposer.exe" %1 /event "%~2" /save %1
+"BridgeComposer.exe" %1 /event "%~2" /save %1
 
 :exitbat
