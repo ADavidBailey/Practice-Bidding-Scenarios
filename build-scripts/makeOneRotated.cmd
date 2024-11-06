e92d159f41e48b1c403698d790d0f022e538ae9a
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-11-06 17:08:05 -0600

    kill chatter from makeOneRotated.cmd

--- build-scripts/makeOneRotated.cmd
+++ build-scripts/makeOneRotated.cmd
@@ -12,11 +12,11 @@ SET oLIN=P:\lin-rotated-for-4-players\%scenarioName%.lin
 :: Rotate the hands N,E,S,W - this will create rotated files in the same folder
 :: create both pbn and lin files in the P:\pbn folder with -NESW.pbn and -NESW.lin appended
 
-cscript S:\SetDealerMulti.js %iPBN%.pbn NESW Dealer
-cscript S:\SetDealerMulti.js %iPBN%.pbn NESW Dealer NoPBN LIN 
+cscript S:\SetDealerMulti.js %iPBN%.pbn NESW Dealer /noui >nul
+cscript S:\SetDealerMulti.js %iPBN%.pbn NESW Dealer NoPBN LIN /noui >nul
 
 :: Move the rotated files to the {pbn/lin}-rotated-for-4-players\ folders and strip the NESW 
-move /Y %iPBN%-NESW.pbn %oPBN%
-move /Y %iPBN%-NESW.lin %oLIN%
+move /Y %iPBN%-NESW.pbn %oPBN% >nul
+move /Y %iPBN%-NESW.lin %oLIN% >nul
 
 :exitbat
