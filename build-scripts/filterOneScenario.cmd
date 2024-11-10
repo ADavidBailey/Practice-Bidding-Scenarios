aa004f97f09ebfff60b9fcc80191ceca26835c31
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-11-10 09:39:03 -0600

    append to cscript... /noui >nul

--- build-scripts/filterOneScenario.cmd
+++ build-scripts/filterOneScenario.cmd
@@ -55,9 +55,7 @@ exit /b
 
 :foundFilter
 
-::  /noui >nul
-
-cscript /nologo S:\Filter.js P:\bba\%scenario%.pbn %this_filter% P:\bba-filtered\%scenario%.pbn --PDF
-cscript /nologo S:\Filter.js P:\bba\%scenario%.pbn %this_filter% P:\bba-filtered-out\%scenario%.pbn --INVERSE --PDF
+cscript /nologo S:\Filter.js P:\bba\%scenario%.pbn %this_filter% P:\bba-filtered\%scenario%.pbn --PDF /noui >nul
+cscript /nologo S:\Filter.js P:\bba\%scenario%.pbn %this_filter% P:\bba-filtered-out\%scenario%.pbn --INVERSE --PDF /noui >nul
 
 exit /b
