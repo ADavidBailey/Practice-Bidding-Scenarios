392412eb81b7801f619ce62198a13bd905171d48
David Bailey <146357155+ADavidBailey@users.noreply.github.com>
2024-11-27 13:21:23 -0600

    deleted retired code.  Installed Minorwood.

--- build-scripts/retired/abbTest.ps1
+++
@@ -1,8 +0,0 @@
-# A called ps1
-$fileCount = 1
-
-$nFiles = $global:nTestFiles
-if ($fileCount -eq $nFiles) { break }
-
-# The number of files for PowerScript programs
-Write-Host "This is a Called ps1 program speaking. nFiles =" $nFiles
